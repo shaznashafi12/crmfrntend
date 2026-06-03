7154696511bc56bffddce5303b34c7cd8af1b4fd
shaznashafi12 <shaznashafi55@gmail.com>
2026-06-03 14:07:01 +0530

    hheh

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./context/Authcontext.jsx";
 import { Toaster } from "react-hot-toast";
 
 import Signup from "./pages/Signup.jsx";
